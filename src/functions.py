99c3a647fea42cbdebe135f7c9738a82e8758116
mesterhammerfic <mesterhammerfic@gmail.com>
2020-08-10 21:42:39 -0500

    cleaned up base model notebook

--- src/functions.py
+++ src/functions.py
@@ -11,28 +11,28 @@ def load_data_generators():
     Returns 3 keras image data generator objects that pull from the /data 
     folder of this directory.
     """
-    train_datagen = image.ImageDataGenerator(horizontal_flip=True, 
+    train_data_generator = image.ImageDataGenerator(horizontal_flip=True, 
                                              zoom_range=0.2,
                                              rescale=1./225)
-    val_datagen = image.ImageDataGenerator(rescale=1./225)
-    directory_iteratorgen = image.ImageDataGenerator(rescale=1./225)
+    val_data_generator = image.ImageDataGenerator(rescale=1./225)
+    test_data_generator = image.ImageDataGenerator(rescale=1./225)
 
-    train_data = train_datagen.flow_from_directory('../../data/chest_xray/train/',
+    train_gen = train_data_generator.flow_from_directory('../../data/chest_xray/train/',
                                                    target_size=(100,100),
                                                    batch_size=32,
                                                    class_mode='binary',
                                                    color_mode='grayscale')
-    val_data = val_datagen.flow_from_directory('../../data/chest_xray/val/',
+    val_gen = val_data_generator.flow_from_directory('../../data/chest_xray/val/',
                                                    target_size=(100,100),
                                                    batch_size=32,
                                                    class_mode='binary',
                                                    color_mode='grayscale')
-    directory_iterator = directory_iteratorgen.flow_from_directory('../../data/chest_xray/test//',
+    test_gen = test_data_generator.flow_from_directory('../../data/chest_xray/test//',
                                                  target_size=(100,100),
                                                  batch_size=32,
                                                  class_mode='binary',
                                                  color_mode='grayscale')
-    return train_data, val_data, directory_iterator
+    return train_gen, val_gen, test_gen
 
 
 
